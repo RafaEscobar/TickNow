a673360dade5bd623ba4f306cc4ec347f6401fb3
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-10-16 18:27:04 -0500

    Avance, Se agrega estructura base y opiones-base de DrawerNavigator

--- navigation/DrawerNavigator.tsx
+++ navigation/DrawerNavigator.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import { View } from 'react-native'
 
+// #1 Importamos el -createDrawerNavigator-
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+// #2 Importamos la Screen TabNavigator
+import { TabNavigator } from './TabNavigator'
+
+// #3 Importamos todas las pages
+import { AboutAppPage } from '../src/pages/AboutAppPage'
+import { ContactPage } from '../src/pages/ContactPage'
+
+// #4 Creamos la constante receptora de -createDrawerNavigator-
+const Drawer = createDrawerNavigator();
+
 export const DrawerNavigator = () => {
   return (
-    <View>
-
-    </View>
+    // #5 Abrimos el Drawer.Navigator
+    <Drawer.Navigator>
+      {/* #6 Generamos las SCREENS de enlaces a las pages */}
+      <Drawer.Screen 
+        name='Proyectos'
+        component={ TabNavigator }
+        options={{ headerShown: false }}  
+      />
+      <Drawer.Screen 
+        name='Acerca de esta App' 
+        component={ AboutAppPage }
+      
+      />
+      <Drawer.Screen 
+        name='Contactanos' 
+        component={ ContactPage }
+      
+      />
+    </Drawer.Navigator>
   )
 }
