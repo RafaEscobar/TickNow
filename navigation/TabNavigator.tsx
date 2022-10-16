752f9d32cfa70818e384a3c728fba718cf7f9572
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-10-16 18:19:32 -0500

    Avance, Se agrega estructura base y options-base de TabNavigator

--- navigation/TabNavigator.tsx
+++ navigation/TabNavigator.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import { View } from 'react-native'
 
+// #1 Agregamos la importación de -createBottomTabNavigator-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+// #2 Importamos todas nuestras Screens
+import { ProyectsScreen } from '../src/screens/ProyectsScreen';
+import { HabitsScreen } from '../src/screens/HabitsScreen';
+import { TasksScreen } from '../src/screens/TasksScreen';
+import { PointsScreen } from '../src/screens/PointsScreen';
+
+// #3 Creamos la constante receptora de -createBottomTabNavigator-
+const Tab = createBottomTabNavigator();
+
 export const TabNavigator = () => {
   return (
-    <View>
-
-    </View>
+    // #4 Abrimos el Tab.Navigator
+    <Tab.Navigator>
+      {/* #5 Generamos las Screens */}
+        {/* Screen Proyectos */}
+      <Tab.Screen 
+        name='Proyectos' 
+        component={ ProyectsScreen }
+        options={{ tabBarLabel: 'Proyectos' }} 
+      />
+        {/* Screen Habitos */}
+      <Tab.Screen 
+        name='Hábitos' 
+        component={ HabitsScreen } 
+        options={{ tabBarLabel: 'Hábitos' }}
+      />
+        {/* Screen Tareas */}
+      <Tab.Screen 
+        name='Tareas' 
+        component={ TasksScreen } 
+        options={{ tabBarLabel: 'Tareas' }}
+      />
+        {/* Screen Puntos de productividad */}
+      <Tab.Screen 
+        name='Productividad' 
+        component={ PointsScreen } 
+        options={{ tabBarLabel: 'Productividad' }}
+      />
+    </Tab.Navigator>
   )
 }
